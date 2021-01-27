fd4e6982380e44ac165ccd13894388a5cea15a15
Szymon Pokojski <szymon.pokojski@lppsa.com>
2021-01-27 22:22:42 +0100

    feat(): add services for owner, pet and vet

---
+++ pet-clinic-data/src/main/java/com/sektorsoftware/petclinic/service/VetService.java
@@ -0,0 +1,16 @@
+package com.sektorsoftware.petclinic.service;
+
+import com.sektorsoftware.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findByLastName(String lastName);
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
