c7a9d4d4665f47cb54d07d7cfd7caf0050ed132a
Szymon Pokojski <szymon.pokojski@lppsa.com>
2021-03-24 22:21:34 +0100

    feat(): add PetTypeService

---
+++ pet-clinic-data/src/main/java/com/sektorsoftware/petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.sektorsoftware.petclinic.service;
+
+import com.sektorsoftware.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/sektorsoftware/petclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.sektorsoftware.petclinic.service.map;
+
+import com.sektorsoftware.petclinic.model.PetType;
+import com.sektorsoftware.petclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
